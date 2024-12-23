28b11a4cab340b22cd796c3a0e2d5ccd05bc30a3
Khawla192 <202005171@stu.uob.edu.bh>
2024-12-23 20:58:47 +0300

    modifiy the server.js and the new.ejs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const morgan = require('morgan')
 
 // Models
-const Fruit = require("./models/fruit.js")
+const Fruit = require("./models/fruit")
 
 const app = express()
 
