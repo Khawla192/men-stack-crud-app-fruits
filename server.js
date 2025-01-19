7efcc3ac83d7f4e8b64813df7d605cccba2c0b2d
Khawla192 <202005171@stu.uob.edu.bh>
2025-01-19 21:48:46 +0300

    add the styling and move the new route above other routes

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ require('./config/database')
 const express = require('express')
 const morgan = require('morgan')
 const methodOverride = require("method-override")
+const path = require("path")
 
 // Models
-const Fruit = require("./models/fruit")
+const Fruit = require("./models/fruit.js")
 
 const app = express()
 
@@ -14,6 +15,7 @@ const app = express()
 app.use(methodOverride("_method"))
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
 
 // ROUTES
 // GET /
@@ -24,45 +26,7 @@ app.get("/", async (req, res) => {
 app.get('/fruits', async (req, res) => {
   const allFruits = await Fruit.find()
   res.render('fruits/index.ejs', { fruits: allFruits })
-  // res.send(allFruits)
 })
-
-app.get("/fruits/:fruitId", async (req, res) => {
-  const fruitId = req.params.fruitId
-  const foundFruit = await Fruit.findById(fruitId)
-    res.render('fruits/show.ejs', { fruit: foundFruit })
-    // res.send(fruit)
-    // `This route renders the show page for fruit id: ${req.params.fruitId}!`
-})
-
-
-app.delete("/fruits/:fruitId", async (req, res) => {
-  // res.send("This is the delete route")
-  const fruitId = req.params.fruitId
-  await Fruit.findByIdAndDelete(fruitId)
-  res.redirect("/fruits")
-})
-
-app.get("/fruits/:fruitId/edit", async (req, res) => {
-  const fruitId = req.params.fruitId
-  const foundFruit = await Fruit.findById(fruitId)
-  res.render("fruits/edit.ejs", {
-    fruit: foundFruit,
-  })
-})
-
-app.put("/fruits/:fruitId", async (req, res) => {
-  if (req.body.isReadyToEat === "on") {
-    req.body.isReadyToEat = true
-  } else {
-    req.body.isReadyToEat = false
-  }
-
-  const fruitId = req.params.fruitId
-  await Fruit.findByIdAndUpdate(fruitId, req.body)
-  res.redirect(`/fruits/${fruitId}`)
-})
-
 // GET /fruits/new
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs")
@@ -77,6 +41,36 @@ app.post("/fruits", async (req, res) => {
   }
   await Fruit.create(req.body)
   res.redirect("/fruits")
+})
+
+app.get("/fruits/:fruitId", async (req, res) => {
+  const fruitId = req.params.fruitId
+  const fruit = await Fruit.findById(fruitId)
+  res.render('fruits/show.ejs', { fruit: fruit })
+})
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+  const fruitId = req.params.fruitId
+  await Fruit.findByIdAndDelete(fruitId)
+  res.redirect("/fruits")
+})
+
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const fruitId = req.params.fruitId
+  const fruit = await Fruit.findById(fruitId)
+  res.render("fruits/edit.ejs", { fruit: fruit })
+})
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true
+  } else {
+    req.body.isReadyToEat = false
+  }
+
+  const fruitId = req.params.fruitId
+  await Fruit.findByIdAndUpdate(fruitId, req.body)
+  res.redirect(`/fruits/${fruitId}`)
 })
 
 // LISTENER
